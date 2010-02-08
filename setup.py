802c8dae464c36e0c9122765dc194b7cd8436238
Rıdvan Örsvuran <flasherdn@gmail.com>
2010-02-08 15:59:59 +0200

    updated setup.py;added asma.desktop

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import glob
+import os
 
+def get_icons():
+    ls = []
+    for i in glob.glob("icons/*x*"):
+        ls.append(("share/icons/hicolor/"+os.path.split(i)[1]+"/apps",
+                   glob.glob(i+"/*.png")))
+    return ls
+data_files = [
+    ("share/applications", ["data/asma.desktop"])
+]
+data_files.extend(get_icons())
+print data_files
 from setuptools import setup
+
 setup(name="asma",
-      version="0.1",
+      version="0.2",
+      package_dir={"":"src"},
       packages = ["asma", "asma.addons"],
-      scripts = ["asma.py"],
+      scripts = ["src/asma.py"],
       description= "All System MAnagers",
       author="Rıdvan Örsvuran",
-      author_email="flasherdn@gmail.com"
+      author_email="flasherdn@gmail.com",
+      data_files=data_files
 )
